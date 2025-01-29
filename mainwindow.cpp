306891bd05ecec26c985b1c9eccf942fb674e189
Tepman2 <sethlienemann@gmail.com>
2025-01-28 20:11:31 -0600

    Add problems 5 and 6

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,19 @@
-//#include <iostream>
+#include <QLCDNumber>
+#include <string>
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "problem01.h"
 #include "problem02.h"
 #include "problem03.h"
+#include "problem04.h"
+#include "problem05.h"
+#include "problem06.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    this->ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
@@ -21,10 +25,7 @@ void MainWindow::on_pushButton_1_clicked()
 {
     ProjectEuler::Problem01 solver = ProjectEuler::Problem01(1000);
     solution01 = solver.solve();
-    while(ui->lcdAnswer_1->checkOverflow((int)solution01)) {
-        ui->lcdAnswer_1->setDigitCount(ui->lcdAnswer_1->digitCount() + 1);
-        ui->lcdAnswer_1->adjustSize();
-    }
+    updateDigitCount(ui->lcdAnswer_01, solution01);
     ui->lcdAnswer_1->display((int)solution01);
     ui->lcdAnswer_1->update();
 }
@@ -34,10 +35,7 @@ void MainWindow::on_pushButton_2_clicked()
 {
     ProjectEuler::Problem02 solver = ProjectEuler::Problem02();
     solution02 = solver.solve();
-    while(ui->lcdAnswer_2->checkOverflow((int)solution02)) {
-        ui->lcdAnswer_2->setDigitCount(ui->lcdAnswer_2->digitCount() + 1);
-        ui->lcdAnswer_2->adjustSize();
-    }
+    updateDigitCount(ui->lcdAnswer_02, solution02);
     ui->lcdAnswer_2->display((int)solution02);
     ui->lcdAnswer_2->update();
 }
@@ -46,11 +44,13 @@ void MainWindow::on_pushButton_3_clicked()
 {
     ProjectEuler::Problem03 solver = ProjectEuler::Problem03(600851475143);
     solution03 = solver.solve();
-    while(ui->lcdAnswer_3->checkOverflow((int)solution03)) {
-        ui->lcdAnswer_3->setDigitCount(ui->lcdAnswer_3->digitCount() + 1);
-        ui->lcdAnswer_3->adjustSize();
-    }
+    updateDigitCount(ui->lcdAnswer_03, solution03);
     ui->lcdAnswer_3->display((int)solution03);
     ui->lcdAnswer_3->update();
 }
 
+void updateDigitCount(QLCDNumber * disp, unsigned long long num) {
+    std::string str_num = std::to_string(num);
+    disp->setDigitCount(str_num.length());
+    disp->adjustSize();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLCDNumber>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,15 +20,20 @@ public:
 
 private slots:
     void on_pushButton_1_clicked();
-
     void on_pushButton_2_clicked();
-
     void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
 
 private:
     Ui::MainWindow *ui;
     unsigned long long solution01 = 0;
     unsigned long long solution02 = 0;
     unsigned long long solution03 = 0;
+    unsigned long long solution04 = 0;
+    unsigned long long solution05 = 0;
+    unsigned long long solution06 = 0;
+    void updateDigitCount(QLCDNumber *disp, unsigned long long num);
 };
 #endif // MAINWINDOW_H
